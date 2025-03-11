5439de3f671fb10340b6d8e87561bde2fb5bcfb7
hassiba saidoun <hassiba.saidoun@keyrus.com>
2025-03-11 06:32:11 +0000

    début des tests

--- models/intermediate/localbike_database/int_localbike_database__product.sql
+++ models/intermediate/localbike_database/int_localbike_database__product.sql
@@ -1,16 +1,10 @@
 SELECT 
-pr.product_id,
-pr.product_name,
-pr.brand_id,
-pr.category_id,
-pr.model_year,
-pr.list_price,
-br.brand_id,
 br.brand_name,
-cat.category_id,
 cat.category_name,
 st.store_id,
-coalesce(st.quantity,0)
+sum(pr.list_price),
+sum(coalesce(st.quantity,0)),
+sum(coalesce(st.quantity,0) * pr.list_price ) total_cost
 FROM
 {{ ref('stg_localbike_database__product') }} AS pr
 INNER JOIN
@@ -22,3 +16,7 @@ ON pr.category_id = cat.category_id
 LEFT JOIN
 {{ ref('stg_localbike_database__stock') }} st
 ON pr.product_id = st.product_id
+group by 
+br.brand_name,
+cat.category_name,
+st.store_id
