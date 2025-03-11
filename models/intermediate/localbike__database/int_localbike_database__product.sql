fed445bb517439dd26875add3a89f2b480081361
hassiba saidoun <hassiba.saidoun@keyrus.com>
2025-03-11 23:17:30 +0000

    l

--- models/intermediate/localbike__database/int_localbike_database__product.sql
+++ models/intermediate/localbike__database/int_localbike_database__product.sql
@@ -1,9 +1,17 @@
 SELECT 
 pr.product_id,
-br.brand_name
+br.brand_name,
+cat.category_name,
+st.store_id
 FROM
 {{ ref('stg_localbike_database__product') }} AS pr
 INNER JOIN
 {{ ref('stg_localbike_database__brand') }} br
 ON pr.brand_id = br.brand_id
+INNER JOIN
+{{ ref('stg_localbike_database__category') }} cat
+ON pr.category_id = cat.category_id
+LEFT JOIN
+{{ ref('stg_localbike_database__stock') }} st
+ON pr.product_id = st.product_id
 
